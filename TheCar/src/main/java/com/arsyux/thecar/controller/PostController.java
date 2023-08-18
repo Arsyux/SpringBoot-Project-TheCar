bfdecf6a89a991f8658497736a263cd39be86d53
arsyux <arsyux@gmail.com>
2023-08-19 00:02:20 +0900

    부트 스트랩 추가

---
+++ TheCar/src/main/java/com/arsyux/thecar/controller/PostController.java
@@ -0,0 +1,15 @@
+package com.arsyux.thecar.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PostController {
+
+	// 기본 화면 설정
+	@GetMapping({"", "/"})
+	public String getPostList() {
+		return "index";
+	}
+	
+}
