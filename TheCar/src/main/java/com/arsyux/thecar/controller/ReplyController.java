80cfd5210bc293dcbb12b7634b4ee46b721dac8d
arsyux <arsyux@gmail.com>
2023-09-10 18:54:32 +0900

    Create ReplyController.java

---
+++ TheCar/src/main/java/com/arsyux/thecar/controller/ReplyController.java
@@ -0,0 +1,33 @@
+package com.arsyux.thecar.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.arsyux.thecar.domain.Reply;
+import com.arsyux.thecar.domain.User;
+import com.arsyux.thecar.dto.ResponseDTO;
+import com.arsyux.thecar.service.ReplyService;
+
+@Controller
+public class ReplyController {
+
+	@Autowired
+	private ReplyService replyService;
+	
+	// Reply 엔티티 생성에 필요한 정보들을 인자로 전달하면서 ReplyService 클래스의 insertReply() 메소드를 호출한다.
+	@PostMapping("/reply/{postId}")
+	public @ResponseBody ResponseDTO<?> insertReply(@PathVariable int postId, @RequestBody Reply reply, HttpSession session) {
+		User principal = (User) session.getAttribute("principal");
+		replyService.insertReply(postId, reply, principal);
+		return new ResponseDTO<>(HttpStatus.OK.value(), postId + "번 포스트에 대한 댓글이 등록되었습니다.");
+	}
+	
+	
+}
