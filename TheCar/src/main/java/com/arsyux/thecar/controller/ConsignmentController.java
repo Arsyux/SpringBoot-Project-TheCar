051a2cfe2a728816c7fb28710c3f627740da5f55
arsyux <arsyux@gmail.com>
2023-09-28 07:12:38 +0900

    Update ConsignmentController.java

--- TheCar/src/main/java/com/arsyux/thecar/controller/ConsignmentController.java
+++ TheCar/src/main/java/com/arsyux/thecar/controller/ConsignmentController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ConsignmentController {
 	
+	// 탁송 예약 GetMapping
 	@GetMapping("/consignment/reservationConsignment")
 	public String reservationConsignment() {
 		return "consignment/reservationConsignment";
